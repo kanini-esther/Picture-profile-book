a0a0b4902e5af693b1c2c6422450da18b983d50d
Kanini-esther <esther.njeri.kanini@gmail.com>
2020-07-28 17:10:22 +0300

    first commit

---
+++ app/src/main/java/com/example/picturebook/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.picturebook
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val next=findViewById<Button>(R.id.button3)
+        next.setOnClickListener {
+            val intent=Intent(this,BufalloPicture::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
