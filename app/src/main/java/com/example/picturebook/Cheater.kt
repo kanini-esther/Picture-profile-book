37be3b30974ee69e0c4e29b6adefc71a3e3c2b5e
Kanini-esther <esther.njeri.kanini@gmail.com>
2020-07-31 16:11:08 +0300

    first commit

--- app/src/main/java/com/example/picturebook/Cheater.kt
+++ app/src/main/java/com/example/picturebook/Cheater.kt
@@ -2,10 +2,12 @@ package com.example.picturebook
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Cheater : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cheater)
+        val next : Button=findViewById<Button>(R.id.button5)
     }
 }
--- app/src/main/java/com/example/picturebook/Rhino.kt
+++ app/src/main/java/com/example/picturebook/Rhino.kt
@@ -1,12 +1,19 @@
 package com.example.picturebook
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Rhino : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rhino)
+        val next:Button=findViewById<Button>(R.id.button4)
+        next.setOnClickListener {
+            val intent=Intent(this,Rhino ::class.java)
+            startActivity(intent)
+        }
 
     }
 }
